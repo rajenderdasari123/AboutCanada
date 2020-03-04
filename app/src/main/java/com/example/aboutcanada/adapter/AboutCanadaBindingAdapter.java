35b578e81ae41dd7d7fbe631ebc57345eefa59c1
rajenderdasari123 <dasarirajendertech@gmail.com>
2020-03-04 09:42:00 +0530

    Fixed image loading issue.

--- app/src/main/java/com/example/aboutcanada/adapter/AboutCanadaBindingAdapter.java
+++ app/src/main/java/com/example/aboutcanada/adapter/AboutCanadaBindingAdapter.java
@@ -3,7 +3,6 @@ package com.example.aboutcanada.adapter;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.example.aboutcanada.R;
 
 import androidx.databinding.BindingAdapter;
 
@@ -16,7 +15,7 @@ public class AboutCanadaBindingAdapter {
   public static void loadImage(ImageView imageView, String imageurl) {
     if (imageurl != null && !imageurl.isEmpty()) {
       Glide.with(imageView.getContext())
-          .load(imageurl).placeholder(R.drawable.ic_launcher).into(imageView);
+          .load(imageurl).into(imageView);
     }
   }
 }
